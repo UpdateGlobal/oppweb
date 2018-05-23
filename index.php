b833f7da7e2727719c1a51fa97b3af0ef19eb915
Frontend Update <arte@update.pe>
2018-05-23 11:56:01 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <!-- The above 3 meta tags *must* come first in the head -->
+
+	<title>Opp - Inmuebles</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <link rel=”canonical” href=””/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+    <!-- twitter card starts from here, if you don't need remove this section -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@yourtwitterusername" />
+    <meta name="twitter:creator" content="@yourtwitterusername" />
+    <meta name="twitter:url" content="http://yourdomain.com" />
+    <meta name="twitter:title" content="Your home page title, max 140 char" /> <!-- maximum 140 char -->
+    <meta name="twitter:description" content="Your site description, maximum 140 char " /> <!-- maximum 140 char -->
+    <meta name="twitter:image" content="assets/img/twittercardimg/twittercard-280-150.jpg" />  <!-- when you post this page url in twitter , this image will be shown -->
+    <!-- twitter card ends from here -->
+
+    <!-- facebook open graph starts from here, if you don't need then delete open graph related  -->
+    <meta property="og:title" content="Your home page title" />
+    <meta property="og:url" content="http://your domain here.com" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:site_name" content="Your site name here" />
+    <!--meta property="fb:admins" content="" /-->  <!-- use this if you have  -->
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="assets/img/opengraph/fbphoto.jpg" /> <!-- when you post this page url in facebook , this image will be shown -->
+    <!-- facebook open graph ends from here -->
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i,600,600i,700" rel="stylesheet">
+
+    <!-- fontawason-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/style.css">
+   <!--  <link rel="stylesheet" href="css/bootstrap-theme.css"/> -->
+
+
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+</head>
+<body>
+
+
+
+<!--topmenu-->
+<div class="top_menu">
+    <div class="container_fluid">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-8">Calle Aldabas Nº.559 of.403 - Surco |  2751241/ 2751254/ 6237723</div>
+        </div>
+    </div>
+</div>
+<!--topmenu-->
+
+
+<!--menu-->
+<nav class="navbar navbar-default">
+  <div class="container-fluid" style="padding: 10px;">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+       <a class="navbar-brand" href="#">
+        <img alt="Brand" class="logo" src="images/pedraza-logo-footer.png" width="220">
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ventas <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Something 1</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated 1</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Alquiler <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Something 2</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated 3</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Proyectos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Something 3</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated 3</a></li>
+          </ul>
+        </li>
+        <li class="lines"><a href="#">Quiero Vender</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href=""><i class="fab fa-whatsapp fa-2x"></i></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--menu-->
+
+
+
+
+<!--slider-->
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6 bg_slide_de">  
+
+            
+        </div>
+        <div class="col-sm-6 bg_slide_iz">
+
+
+            <div class="owl-carousel owl-theme">
+                <div class="item"><img src="images/bitmap.png" class="items"></div>
+                <div class="item"><img src="images/bitmap.png" class="items"></div>
+            </div>
+
+            
+        </div>
+    </div>
+</div>
+
+
+<!--slider-->
+
+
+
+ <!-- Set up your HTML -->
+           
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/npm.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+          $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin:0,
+                autoplay:true,
+                autoplayTimeout:2000,
+                nav:false,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:1
+                    },
+                    1000:{
+                        items:1
+                    }
+                }
+            })
+        });
+    </script>
+
+</body>
+</html>
+
+
+
